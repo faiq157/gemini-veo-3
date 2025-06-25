6f2818b7f4feb7a04f0dbb3219800259ba29f7ca
Faiq Ahmad <77959437+faiq157@users.noreply.github.com>
2025-06-25 14:00:00 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   typescript: {
+    ignoreBuildErrors: true, // ⚠️ Skips type errors during build — use with caution
+  },
 };
 
 export default nextConfig;
